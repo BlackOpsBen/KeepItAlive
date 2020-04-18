e3c7a10e4e1e7d79de969c45150b081233aa2c2f
Ben Mora <blackopsben@gmail.com>
2020-04-18 00:14:30 -0500

    Basic player movement and camera follow with smoothing

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    private Transform player;
+    [SerializeField] private float cameraSmoothness = 5f;
+
+    private void Awake()
+    {
+        player = FindObjectOfType<PlayerMovement>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, player.position, Time.deltaTime * cameraSmoothness);
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeedMultiplier = 10f;
+    [SerializeField] private float turnSpeedMultiplier = 5f;
+    
+    private Transform cameraParent;
+    
+    private Vector2 movementInputAxis;
+
+    private void Awake()
+    {
+        cameraParent = FindObjectOfType<FollowPlayer>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetMovementInput();
+        ProcessMovement();
+    }
+
+    private void GetMovementInput()
+    {
+        movementInputAxis.x = Input.GetAxis("Horizontal");
+        movementInputAxis.y = Input.GetAxis("Vertical");
+        Debug.Log(movementInputAxis);
+    }
+
+    private void ProcessMovement()
+    {
+        // Move
+        Vector3 forward = cameraParent.forward;
+        Vector3 right = cameraParent.right;
+        Vector3 facingDirection = transform.forward * Mathf.Max(Mathf.Abs(movementInputAxis.x), Mathf.Abs(movementInputAxis.y));
+        Vector3 moveDirection = forward * movementInputAxis.y + right * movementInputAxis.x;
+        transform.Translate(facingDirection * moveSpeedMultiplier * Time.deltaTime, Space.World);
+
+        // Rotate to face move direction
+        Quaternion rotation = Quaternion.identity;
+        if (moveDirection != Vector3.zero)
+        {
+            rotation = Quaternion.LookRotation(moveDirection, transform.up);
+        }
+        if (Mathf.Abs(movementInputAxis.x) > float.Epsilon || Mathf.Abs(movementInputAxis.y) > float.Epsilon)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, turnSpeedMultiplier * Time.deltaTime);
+        }
+    }
+}
