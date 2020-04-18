7738206589b1cab540c9f1324ea8e8e31e0905ea
Ben Mora <blackopsben@gmail.com>
2020-04-17 23:47:29 -0500

    Basic world laid out

---
+++ Assets/Scripts/SnapToGrid.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class SnapToGrid : MonoBehaviour
+{
+    private int gridSize = 4;
+
+    void Update()
+    {
+        SnapPosition();
+    }
+
+    private void SnapPosition()
+    {
+        transform.position = new Vector3(
+            Mathf.RoundToInt(transform.position.x / gridSize) * gridSize,
+            0f,
+            Mathf.RoundToInt(transform.position.z / gridSize) * gridSize
+            );
+    }
+}
