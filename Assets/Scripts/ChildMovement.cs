571b3dcffb6afa7f784fbbe40c722ad537310bd6
Ben Mora <blackopsben@gmail.com>
2020-04-18 23:59:24 -0500

    Babies!

---
+++ Assets/Scripts/ChildMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeedMultiplier = 10f;
+    [SerializeField] private float turnSpeedMultiplier = 10f;
+
+    [SerializeField] private Animator animator;
+
+    private Vector2 movementGeneratedAxis;
+
+    private Vector3 moveDirection;
+
+    private bool canMove = true;
+
+    void Update()
+    {
+        GenerateMovementAxis();
+        ProcessMovement();
+    }
+
+    private void GenerateMovementAxis()
+    {
+        movementGeneratedAxis.x = Random.Range(-1f, 1f);
+        movementGeneratedAxis.y = Random.Range(-1f, 1f);
+        animator.SetFloat("Running", movementGeneratedAxis.normalized.magnitude);
+    }
+
+    private void ProcessMovement()
+    {
+        // Move
+        Vector3 forward = Vector3.forward;
+        Vector3 right = Vector3.right;
+        Vector3 facingDirection = transform.forward * Mathf.Max(Mathf.Abs(movementGeneratedAxis.x), Mathf.Abs(movementGeneratedAxis.y));
+        moveDirection = forward * movementGeneratedAxis.y + right * movementGeneratedAxis.x;
+        if (canMove)
+        {
+            transform.Translate(facingDirection * moveSpeedMultiplier * Time.deltaTime, Space.World);
+        }
+
+        // Rotate to face move direction
+        Quaternion rotation = Quaternion.identity;
+        if (moveDirection != Vector3.zero)
+        {
+            rotation = Quaternion.LookRotation(moveDirection, transform.up);
+        }
+        if (Mathf.Abs(movementGeneratedAxis.x) > float.Epsilon || Mathf.Abs(movementGeneratedAxis.y) > float.Epsilon)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, turnSpeedMultiplier * Time.deltaTime);
+        }
+    }
+}
--- Assets/Scripts/UseHouse.cs
+++ Assets/Scripts/UseHouse.cs
@@ -6,6 +6,8 @@ public class UseHouse : MonoBehaviour
 {
     [SerializeField] private float energyCost = 1f;
     [SerializeField] private float feedbackIntensity = 20f;
+    [SerializeField] private GameObject childBoy;
+    [SerializeField] private GameObject childGirl;
     public void OnUseBuilding()
     {
         if (GameManager.Instance.GetCurrentEnergy() >= energyCost)
@@ -17,6 +19,22 @@ public class UseHouse : MonoBehaviour
             AudioManager.Instance.PlaySound("UseHouse");
 
             GameManager.Instance.GainHeart();
+
+            if (Random.Range(0f, 1f) < 0.1f)
+            {
+                GameObject childToBirth;
+                if (Random.Range(0f, 1f) < 0.5f)
+                {
+                    childToBirth = childBoy;
+                }
+                else
+                {
+                    childToBirth = childGirl;
+                }
+                Vector3 spawnPoint = new Vector3(transform.position.x + Random.Range(-1f, 1f), transform.position.y, transform.position.z + Random.Range(-1f, 1f)) * 3f;
+                Instantiate(childToBirth, transform.position, Quaternion.identity);
+                AudioManager.Instance.PlaySound("Baby");
+            }
         }
         else
         {
