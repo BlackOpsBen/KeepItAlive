761eee9db9bd9fb5ee12499ba091cbcf2eca0de9
Ben Mora <blackopsben@gmail.com>
2020-04-19 17:34:15 -0500

    Foot step sounds added

---
+++ Assets/FootSteps.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootSteps : MonoBehaviour
+{
+    [SerializeField] GameObject footStepSoundObject;
+    public void FootStep()
+    {
+        Instantiate(footStepSoundObject, transform.position, Quaternion.identity);
+    }
+}
---
+++ Assets/RandomPitch.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPitch : MonoBehaviour
+{
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] float pitchShiftMax = 0.1f;
+    void Awake()
+    {
+        audioSource.pitch += UnityEngine.Random.Range(-pitchShiftMax, pitchShiftMax);
+    }
+}
